4b7d1e66cec25549ef6dfc906d1b066eafa393b1
Gustavo A. Lima <gustavo.lima@pmweb.com.br>
2020-03-11 17:32:57 -0300

    inclui pdf miner

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("hello")
+from pdfminer import high_level
+from os import listdir 
+
+lista_pdfs = list(filter(lambda x: x.endswith('.pdf'), listdir('notas_clear/')))
+lista_pdfs = list(map(lambda x: 'notas_clear/' + x, lista_pdfs))
+
+for pdf in lista_pdfs:
+	print(high_level.extract_text(pdf))

defe49156a2c97b789a5c5fd69934eae7d015a3d
Gustavo A. Lima <gustavo.lima@pmweb.com.br>
2020-03-16 17:51:19 -0300

    Inclui draft no script_tabula.py

--- script_tabula.py
+++ script_tabula.py
@@ -1,5 +1,16 @@
-from tabula import convert_into_by_batch 
+from os import listdir
+from tabula import read_pdf 
 
-# pdf = read_pdf("notas_clear", batch="notas_clear/", area=(237.628,33.841,445.134,558.184))
-pdfs = convert_into_by_batch("notas_clear/", output_format="csv")
-print(pdfs)
+lista_pdfs = list(filter(lambda x: x.endswith('.pdf'), listdir('notas_clear/')))
+lista_pdfs = list(map(lambda x: 'notas_clear/' + x, lista_pdfs))
+
+notas_clear = []
+
+for pdf in lista_pdfs:	
+	notas_clear.append(read_pdf(pdf, area=(237.628,33.841,445.134,558.184)))	
+
+# pdfs = convert_into_by_batch("notas_clear/", output_format="csv")
+
+
+
+
